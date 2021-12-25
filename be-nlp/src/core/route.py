6b963c9ec1c2c8e3208d4546950db1f47113baeb
TruongTD1112 <truong.td173429@sis.hust.edu.vn>
2021-12-26 06:00:37 +0700

    chinh sua lai preprocessing

--- be-nlp/src/core/route.py
+++ be-nlp/src/core/route.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from reference import add_diacritic
+from validate import preprocessing
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
@@ -11,10 +12,34 @@ def hello_world():
     string = request.json['string']
     restore_tone = request.json['restore_tone']
     keep_special_character = request.json['keep_special_character']
-
-    result = add_diacritic(string, restore_tone, keep_special_character)
+    totalWord = len(string.split(" "))
+    preProcess = preprocessing(string)
+    stringPreProcess = preProcess[0]
+    stringPredict = add_diacritic(stringPreProcess, False, True)
+    result = getResult(stringPredict, preProcess[1], preProcess[2], totalWord)
     return jsonify({"result" : result})
+
+
+def getResult(stringPredict, listStringAfterRemove, listStringRemove, length):
+    print(listStringRemove)
+    resultArray = [None] * length
+    print(resultArray)
+    stringPredictSplit = stringPredict.split(" ")
+    listStringAfterRemovePredict = []
+    for i in range(len(listStringAfterRemove)):
+        listStringAfterRemovePredict.append((listStringAfterRemove[i][1], stringPredictSplit[i]))
+    # print(listStringAfterRemovePredict)
+    for i in range(len(listStringAfterRemovePredict)):
+        resultArray[listStringAfterRemovePredict[i][0]] =  listStringAfterRemovePredict[i][1]
+    for i in range(len(listStringRemove)):
+        resultArray[listStringRemove[i][1]] =  listStringRemove[i][0]
+
+    result = ""
+    for i in range(length):
+        result += resultArray[i] + " "
+    return result
 
 
 if __name__ == '__main__':
     app.run()
+
--- be-nlp/src/core/validate.py
+++ be-nlp/src/core/validate.py
@@ -2,6 +2,8 @@ import codecs
 import csv
 import re
 import sys
+import string
+import re
 
 def remove_tone_line(s):
   # Trong tiếng Việt chỉ thêm dấu vào nguyên âm và y, d
@@ -19,3 +21,58 @@ def remove_tone_line(s):
   # Thay thế các từ có dấu xuất hiện trong tìm kiếm của regex bằng từ không dấu tương ứng
   return r.sub(lambda m: replace_dict[m.group()], s)
   # m là kết quả của so khớp, m.group trả về các phần tử khớp với pattern
+
+# https://realpython.com/python-encodings-guide/
+# List các ký tự hợp lệ trong tiếng Việt
+intab_l = "ạảãàáâậầấẩẫăắằặẳẵóòọõỏôộổỗồốơờớợởỡéèẻẹẽêếềệểễúùụủũưựữửừứíìịỉĩýỳỷỵỹđ"
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+whitespace = ' '
+accept_strings =  intab_l + ascii_lowercase + digits + punctuation + whitespace
+r = re.compile('^[' + accept_strings + ']+$')
+
+
+# Một câu sẽ được coi là hợp lệ nếu có các ký tự nằm trong accept_strings
+def _check_tieng_viet(seq):
+  if re.match(r, seq.lower()):
+    return True
+  else:
+    return False
+
+
+def preprocessing(string):
+  listStringSplit = string.split(" ")
+  indexInit = []
+  for i in range(len(listStringSplit)):
+    indexInit.append(i)
+
+  init = list(zip(listStringSplit, indexInit))
+
+  listStringAfterRemove = []
+  listStringRemove = []
+  for index, value in enumerate(init):
+    if(_check_tieng_viet(value[0])):
+      listStringAfterRemove.append((value))
+    else:
+      listStringRemove.append(value)
+  # print(listStringAfterRemove)
+  # print(listStringRemove)
+  stringAfterRemove = ""
+  for index, value in enumerate(listStringAfterRemove):
+    stringAfterRemove += value[0] + " "
+
+  return [stringAfterRemove, listStringAfterRemove, listStringRemove, len(listStringSplit)]
+
+
+# def getResult(string):
+#   preProcess = preprocessing(string)
+#   stringPredict = add_diacritic()
+#   stringPreProcess = preProcess[0]
+
+
+
+
+
+# print(init)
+# print(_check_tieng_viet('tiếng'))
